317c0301ecb9ed9624d38b7df6910d84b07c9c05
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-02-22 10:42:29 -0600

    Add country breakdown.

--- queries.sql
+++ queries.sql
@@ -15,29 +15,52 @@ ORDER BY year;
 
 SELECT
     year,
-    COUNT(CASE WHEN launch_status = 'S' THEN tag END) AS successful_launches,
-    COUNT(CASE WHEN launch_status = 'F' THEN tag END) AS failed_launches
+    COUNT(*) as total,
+    COUNT(CASE WHEN launch_status = 'S' THEN tag END) AS success,
+    COUNT(CASE WHEN launch_status = 'F' THEN tag END) AS failure,
+    COUNT(CASE WHEN launch_status = 'U' THEN tag END) AS unknown
 FROM launches
 GROUP BY year
 ORDER BY year;
 
-.output results/orbital_failures_by_year.csv
+.output results/success_by_year.csv
 
 SELECT
     year,
-    COUNT(CASE WHEN launch_status = 'S' THEN tag END) AS successful_launches,
-    COUNT(CASE WHEN launch_status = 'F' THEN tag END) AS failed_launches
+    COUNT(*) as total,
+    COUNT(CASE WHEN launch_status = 'S' THEN tag END) AS success,
+    COUNT(CASE WHEN launch_status = 'F' THEN tag END) AS failure,
+    COUNT(CASE WHEN launch_status = 'U' THEN tag END) AS unknown
 FROM launches
 WHERE launch_type = 'O'
 GROUP BY year
 ORDER BY year;
 
-.output results/commercial_by_year.csv
+.output results/class_by_year.csv
 
 SELECT
     year,
-    COUNT(CASE WHEN class = 'B' THEN tag END) AS commercial_launches,
-    COUNT(CASE WHEN class != 'B' THEN tag END) AS non_commercial_launches
+    COUNT(*) as total,
+    COUNT(CASE WHEN class = 'A' THEN tag END) AS academic,
+    COUNT(CASE WHEN class = 'B' THEN tag END) AS business,
+    COUNT(CASE WHEN class = 'C' THEN tag END) AS military,
+    COUNT(CASE WHEN class = 'D' THEN tag END) AS government
+FROM
+    launches,
+    orgs
+WHERE launches.agency = orgs.code
+GROUP BY year
+ORDER BY year;
+
+.output results/total_by_country_by_year.csv
+
+SELECT
+    year,
+    COUNT(*) as total,
+    COUNT(CASE WHEN state_code = 'US' THEN tag END) AS united_states,
+    COUNT(CASE WHEN state_code = 'SU' OR state_code = 'RU' THEN tag END) AS russia,
+    COUNT(CASE WHEN state_code = 'CN' THEN tag END) AS china,
+    COUNT(CASE WHEN state_code = 'IN' THEN tag END) AS india
 FROM
     launches,
     orgs
